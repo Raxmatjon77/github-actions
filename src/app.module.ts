6f4a14c2211b239b640ec3805aaa2073e9f169e9
raxmatjon <raxmatjonhamidov242@gmail.com>
2024-08-24 00:57:54 +0500

    to check monitoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,19 @@ import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from './prisma/prisma.module';
 import { MainappModule } from './mainapp/mainapp.module';
-
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 @Module({
-  imports: [AuthModule, PrismaModule, MainappModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    MainappModule,
+    PrometheusModule.register({
+      path: '/metrics', // default is /metrics
+      defaultMetrics: {
+        enabled: true, // Enable the collection of default metrics
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
