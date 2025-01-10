267237b04510ece913c644899edd47ffa03f1a55
Hamidov Raxmatjon <hamidovraxmatjon@Hamidovs-MacBook-Pro.local>
2025-01-10 17:50:48 +0500

    changed

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'dotenv';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggerMiddleware } from './common/middlewares/logger';
 
-config();
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    // logger: false,
+  });
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('The API description for the project ')
     .setVersion('1.0')
     .addTag('api')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.use(new LoggerMiddleware().use);
   app.useGlobalPipes(new ValidationPipe());
   const PORT = process.env.PORT;
-  await app.listen(3000, () => {
-    console.log(`server started on port  ${3000}`);
-  });
+  await app.listen(3000);
 }
 
 bootstrap();
