f8cb509501677a7bd9f62edcff16fc3e9a4acbe3
Hamidov Raxmatjon <hamidovraxmatjon@Hamidovs-MacBook-Pro.local>
2025-01-18 15:18:30 +0500

    console shaxboz

--- src/common/middlewares/logger.ts
+++ src/common/middlewares/logger.ts
@@ -7,7 +7,8 @@ export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction): void {
     // Create a morgan logger instance
     const logger = morgan(':method :url :status :response-time ms ');
-
+    console.log('shaxboz');
+    
     // Use the logger to log the request details
     logger(req, res, (err) => {
       if (err) return next(err);
