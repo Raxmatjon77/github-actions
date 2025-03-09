216bdfd00db05e468af7b66a09dce04e93388fd5
Hamidov Raxmatjon <hamidovraxmatjon@Hamidovs-MacBook-Pro.local>
2025-03-09 23:19:26 +0500

    logger

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggerInterceptor } from './interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('API Documentation')
-    .setDescription('The API description for the project ')
-    .setVersion('1.0')
-    .addTag('api')
-    .build();
 
   // app.useLogger(['log', 'error', 'warn', 'debug', 'verbose']);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LoggerInterceptor());
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
   // const PORT = process.env.PORT;
   console.log('suuui');
